73fa23d41f014fe17b7ce46a39ebfe7d836c698d
Julia Ero <150908837+juli-hellobetter@users.noreply.github.com>
2025-07-15 13:04:02 +0000

    adjust lessons in test

--- api/integrationTests/intervention.test.ts
+++ api/integrationTests/intervention.test.ts
@@ -1,31 +1,33 @@
 import request from "supertest";
-import { expect } from "chai"
+import { expect } from "chai";
 import { buildServer } from "../buildServer";
-import { Express } from "express"
+import { Express } from "express";
 import { cleanDb, closeDbConnection } from "../config/database";
 import mongoose from "mongoose";
 import { createIntervention } from "./factories/interventionFactory";
 
 describe("GET /intervention/:id", () => {
-  let app: Express
+  let app: Express;
 
   before(async () => {
-   app = await buildServer()
-  })
+    app = await buildServer();
+  });
 
   after(async () => {
-    await closeDbConnection()
-  })
+    await closeDbConnection();
+  });
 
   afterEach(async () => {
-    await cleanDb()
-  })
+    await cleanDb();
+  });
 
   describe("when intervention exists", () => {
     it("should return 200 status and intervention data", async () => {
-      const intervention = await createIntervention()
+      const intervention = await createIntervention();
 
-      const res = await request(app).get(`/v1/intervention/${intervention._id.toString()}`);
+      const res = await request(app).get(
+        `/v1/intervention/${intervention._id.toString()}`,
+      );
 
       expect(res.status).to.equal(200);
       expect(res.body).to.deep.equal({
@@ -34,22 +36,26 @@ describe("GET /intervention/:id", () => {
         version: intervention.version,
         name: intervention.name,
         status: intervention.status,
-        lessons: intervention.lessons.map(lesson => ({
+        lessons: intervention.lessons.map((lesson) => ({
           _id: lesson._id.toString(),
           description: lesson.description,
-          title: lesson.title
+          title: lesson.title,
+          completed: lesson.completed,
+          progress: lesson.progress,
         })),
         createdAt: intervention.createdAt.toLocaleDateString("en-GB"),
-        updatedAt:  intervention.createdAt.toLocaleDateString("en-GB")
-      })
+        updatedAt: intervention.createdAt.toLocaleDateString("en-GB"),
+      });
     });
-  })
+  });
 
   describe("when intervention does not exist", () => {
     it("should return 404 status", async () => {
-      const res = await request(app).get(`/v1/intervention/${new mongoose.Types.ObjectId()}`);
+      const res = await request(app).get(
+        `/v1/intervention/${new mongoose.Types.ObjectId()}`,
+      );
 
       expect(res.status).to.equal(404);
-    })
-  })
+    });
+  });
 });
